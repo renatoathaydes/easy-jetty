2dcf90f08e90a64879d628c585c6089f7ac6a29f
Renato Athaydes <renatoathaydes@hotmail.com>
2015-01-10 00:04:42 +0100

    Added util methods for creating MethodArbiters

--- src/main/java/com/athaydes/easyjetty/http/MethodArbiter.java
+++ src/main/java/com/athaydes/easyjetty/http/MethodArbiter.java
@@ -1,5 +1,9 @@
 package com.athaydes.easyjetty.http;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * An arbiter to define whether a HTTP Method should be accepted or not.
  * <p/>
@@ -28,16 +32,54 @@ public interface MethodArbiter {
             return name().equals(method);
         }
 
+        /**
+         * Creates and returns a {@link com.athaydes.easyjetty.http.MethodArbiter}
+         * that accepts any HTTP method.
+         *
+         * @return MethodArbiter
+         */
+        public static MethodArbiter anyMethod() {
+            return new AnyMethodArbiter();
+        }
+
+        /**
+         * Creates and returns a {@link com.athaydes.easyjetty.http.MethodArbiter}
+         * that accepts any HTTP method.
+         *
+         * @return MethodArbiter
+         */
+        public static MethodArbiter anyOf(Method accepted, Method... othersAccepted) {
+            return new AnyOfMethodsArbiter(accepted, othersAccepted);
+        }
+
     }
 
-    /**
-     * An AcceptMethod that accepts any Method.
-     */
-    public static class AnyMethodArbiter implements MethodArbiter {
+    static class AnyMethodArbiter implements MethodArbiter {
         @Override
         public boolean accepts(String method) {
             return true;
         }
+    }
+
+    static class AnyOfMethodsArbiter implements MethodArbiter {
+
+        private final Set<Method> acceptedMethods;
+
+        private AnyOfMethodsArbiter(Method accepted, Method... othersAccepted) {
+            acceptedMethods = new HashSet<>(othersAccepted.length + 1);
+            acceptedMethods.add(accepted);
+            acceptedMethods.addAll(Arrays.asList(othersAccepted));
+        }
+
+        @Override
+        public boolean accepts(String method) {
+            try {
+                return acceptedMethods.contains(Method.valueOf(method));
+            } catch (IllegalArgumentException e) {
+                return false;
+            }
+        }
+
     }
 
 }
