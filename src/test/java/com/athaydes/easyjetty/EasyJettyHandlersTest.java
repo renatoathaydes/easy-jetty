fc65c5b8c2b482fcbf915a56bbe5a46cef6f755c
Renato Athaydes <renatoathaydes@hotmail.com>
2015-01-15 22:03:59 +0100

    Adding support for path parameters - PART 3 - added test with thousands of paths

--- src/test/java/com/athaydes/easyjetty/EasyJettyHandlersTest.java
+++ src/test/java/com/athaydes/easyjetty/EasyJettyHandlersTest.java
@@ -8,10 +8,17 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
-import static com.athaydes.easyjetty.http.MethodArbiter.Method.*;
+import static com.athaydes.easyjetty.http.MethodArbiter.Method.GET;
+import static com.athaydes.easyjetty.http.MethodArbiter.Method.OPTIONS;
+import static com.athaydes.easyjetty.http.MethodArbiter.Method.anyMethod;
+import static com.athaydes.easyjetty.http.MethodArbiter.Method.anyOf;
+import static com.athaydes.easyjetty.http.MethodArbiter.Method.customMethod;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class EasyJettyHandlersTest extends EasyJettyTest {
 
@@ -123,6 +130,89 @@ public class EasyJettyHandlersTest extends EasyJettyTest {
         assertEquals(HttpStatus.METHOD_NOT_ALLOWED_405, putEx.getResponseStatus());
         assertEquals(HttpStatus.NOT_FOUND_404, postEx.getResponseStatus());
         assertEquals(HttpStatus.METHOD_NOT_ALLOWED_405, delEx.getResponseStatus());
+    }
+
+    @Test
+    public void largeNumberOfHandlersTest() throws Exception {
+        final List<String> subPaths = Arrays.asList("hej", "hello", "ola");
+        final int maxIs = 100;
+        final int maxJs = 10;
+
+        class Data {
+            int i, j;
+            String p;
+
+            Data(int i, int j, String p) {
+                this.i = i;
+                this.j = j;
+                this.p = p;
+            }
+        }
+
+        abstract class ToRun {
+            abstract boolean run(Data data);
+        }
+
+        class Runner {
+            void start(ToRun toRun) {
+                mainLoop:
+                for (int i = 0; i < maxIs; i++) {
+                    for (int j = 0; j < maxJs; j++) {
+                        for (final String p : subPaths) {
+                            boolean shouldContinue = toRun.run(new Data(i, j, p));
+                            if (!shouldContinue) {
+                                break mainLoop;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // GIVEN a large number of paths with corresponding handlers
+        new Runner().start(new ToRun() {
+            @Override
+            boolean run(final Data data) {
+                easy.on(GET, "/index" + data.i + "/" + data.p + data.j, new Response() {
+                    @Override
+                    public void respond(Exchange exchange) throws IOException {
+                        exchange.out.println("index" + data.i + " j" + data.j + data.p);
+                    }
+                }).on(GET, "/sub" + data.i + "/j" + data.j + "/" + data.p, new Response() {
+                    @Override
+                    public void respond(Exchange exchange) throws IOException {
+                        exchange.out.println("sub" + data.i + " j" + data.j + data.p);
+                    }
+                });
+                return true;
+            }
+        });
+
+        easy.start();
+
+        // WHEN a GET request is sent out to each one of the handlers
+        final AtomicReference<Throwable> error = new AtomicReference<>();
+        new Runner().start(new ToRun() {
+            @Override
+            boolean run(final Data data) {
+                try {
+                    ContentExchange exchange1 = sendReqAndWait("GET", "http://localhost:8080/index" + data.i + "/" + data.p + data.j);
+                    assertEquals(HttpStatus.OK_200, exchange1.getResponseStatus());
+                    assertEquals("index" + data.i + " j" + data.j + data.p, exchange1.getResponseContent().trim());
+
+                    ContentExchange ex2 = sendReqAndWait("GET", "http://localhost:8080/sub" + data.i + "/j" + data.j + "/" + data.p);
+                    assertEquals(HttpStatus.OK_200, ex2.getResponseStatus());
+                    assertEquals("sub" + data.i + " j" + data.j + data.p, ex2.getResponseContent().trim());
+                    return true;
+                } catch (Throwable t) {
+                    error.set(t);
+                    return false;
+                }
+            }
+        });
+
+        // THEN no error is thrown, all requests completed and passed all assertions
+        assertNull(error.get());
     }
 
 }
