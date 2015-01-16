7b2778b92d9891f66c9d813cb4de58c658250114
Renato Athaydes <renatoathaydes@hotmail.com>
2015-01-16 21:59:51 +0100

    Small addition to easyjetty.groovy

--- src/demo/easyjetty.groovy
+++ src/demo/easyjetty.groovy
@@ -1,7 +1,11 @@
 @Grab('com.athaydes.easy-jetty:easy-jetty:0.1')
 import com.athaydes.easyjetty.EasyJetty
-
 import static com.athaydes.easyjetty.http.MethodArbiter.Method.GET
 
-new EasyJetty().on(GET, "/hello",
+easy = new EasyJetty().on(GET, "/hello",
         { e -> e.out.println 'Hello World!' }).start()
+
+println "Hit any key to stop the server!"
+System.in.read()
+
+easy.stop()
